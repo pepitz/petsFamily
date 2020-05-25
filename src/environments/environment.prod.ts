5952e88ca01d5a9796f3cb9a465a8d8c26f7f27d
Petar Tsenev <petar.tzenev@gmail.com>
2020-05-25 14:11:32 +0200

    fix environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  base_url: "https://my-json-server.typicode.com/Feverup/fever_pets_data/pets/",;
+  base_url: "https://my-json-server.typicode.com/Feverup/fever_pets_data/pets/",
 };
